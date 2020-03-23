dc75cb20aa5c6f2363cca507ed9e641a86186530
hard-coded <cozyncoze@gmail.com>
2020-03-23 11:40:20 -0400

    commented out temporary

--- Assets/Scripts/Network/NetworkClient.cs
+++ Assets/Scripts/Network/NetworkClient.cs
@@ -5,103 +5,106 @@ using UnityEngine;
 using Unity.Collections;
 using Unity.Networking.Transport;
 
-public class NetworkClient : MonoBehaviour
+namespace OldNetwork
 {
-    public UdpNetworkDriver m_Driver;
-    public NetworkConnection m_Connection;
-    public string m_ServerIp = "3.219.69.41";
-    public ushort m_ServerPort = 12345;
-    public bool m_Done;
+    //public class NetworkClient : MonoBehaviour
+    //{
+        //public UdpNetworkDriver m_Driver;
+        //public NetworkConnection m_Connection;
+        //public string m_ServerIp = "3.219.69.41";
+        //public ushort m_ServerPort = 12345;
+        //public bool m_Done;
 
-    void Start ()
-    {
-        m_Driver = new UdpNetworkDriver(new INetworkParameter[0]);
-        m_Connection = default(NetworkConnection);
+        //void Start()
+        //{
+        //    m_Driver = new UdpNetworkDriver( new INetworkParameter[0] );
+        //    m_Connection = default( NetworkConnection );
 
-        var endpoint = NetworkEndPoint.Parse( m_ServerIp, m_ServerPort );
-        m_Connection = m_Driver.Connect(endpoint);
-        //InvokeRepeating( "SendPlayerPacket", 1.0f, 1.0f / 30.0f );
-    }
+        //    var endpoint = NetworkEndPoint.Parse( m_ServerIp, m_ServerPort );
+        //    m_Connection = m_Driver.Connect( endpoint );
+        //    //InvokeRepeating( "SendPlayerPacket", 1.0f, 1.0f / 30.0f );
+        //}
 
-    public void OnDestroy()
-    {
-        m_Driver.Dispose();
-    }
+        //public void OnDestroy()
+        //{
+        //    m_Driver.Dispose();
+        //}
 
-    void Update()
-    {
-        m_Driver.ScheduleUpdate().Complete();
+        //void Update()
+        //{
+        //    m_Driver.ScheduleUpdate().Complete();
 
-        if (!m_Connection.IsCreated)
-        {
-            if (!m_Done)
-                Debug.Log("Something went wrong during connect");
-            return;
-        }
+        //    if( !m_Connection.IsCreated )
+        //    {
+        //        if( !m_Done )
+        //            Debug.Log( "Something went wrong during connect" );
+        //        return;
+        //    }
 
-        DataStreamReader stream;
-        NetworkEvent.Type cmd;
+        //    DataStreamReader stream;
+        //    NetworkEvent.Type cmd;
 
-        while ((cmd = m_Connection.PopEvent(m_Driver, out stream)) !=
-               NetworkEvent.Type.Empty)
-        {
-            if (cmd == NetworkEvent.Type.Connect)
-            {
-                Debug.Log("We are now connected to the server : " + m_Connection.InternalId );
+        //    while( ( cmd = m_Connection.PopEvent( m_Driver, out stream ) ) !=
+        //           NetworkEvent.Type.Empty )
+        //    {
+        //        if( cmd == NetworkEvent.Type.Connect )
+        //        {
+        //            Debug.Log( "We are now connected to the server : " + m_Connection.InternalId );
 
-                using (var writer = new DataStreamWriter(4, Allocator.Temp))
-                {
-                    writer.Write( m_Connection.InternalId );
-                    m_Connection.Send(m_Driver, writer);
-                }
-            }
-            else if (cmd == NetworkEvent.Type.Data)
-            {
-                var readerCtx = default(DataStreamReader.Context);
-                int numBytes = stream.GetBytesRead( ref readerCtx );
-                byte[] bytes = stream.ReadBytesAsArray( ref readerCtx, numBytes );
-                string jsonText = System.Text.Encoding.ASCII.GetString( bytes );
-                PlayerInfoData[] playerInfo = JsonUtility.FromJson<PlayerInfoData[]>(jsonText);
+        //            using( var writer = new DataStreamWriter( 4, Allocator.Temp ) )
+        //            {
+        //                writer.Write( m_Connection.InternalId );
+        //                m_Connection.Send( m_Driver, writer );
+        //            }
+        //        }
+        //        else if( cmd == NetworkEvent.Type.Data )
+        //        {
+        //            var readerCtx = default( DataStreamReader.Context );
+        //            int numBytes = stream.GetBytesRead( ref readerCtx );
+        //            byte[] bytes = stream.ReadBytesAsArray( ref readerCtx, numBytes );
+        //            string jsonText = System.Text.Encoding.ASCII.GetString( bytes );
+        //            PlayerInfoData[] playerInfo = JsonUtility.FromJson<PlayerInfoData[]>( jsonText );
 
-                Debug.Log("Got the value = " + bytes + " from the server : " + jsonText );
-                m_Done = true;
-                //m_Connection.Disconnect(m_Driver);
-                //m_Connection = default(NetworkConnection);
-            }
-            else if (cmd == NetworkEvent.Type.Disconnect)
-            {
-                Debug.Log("Client got disconnected from server");
-                m_Connection = default(NetworkConnection);
-            }
-        }
-    }
+        //            Debug.Log( "Got the value = " + bytes + " from the server : " + jsonText );
+        //            m_Done = true;
+        //            //m_Connection.Disconnect(m_Driver);
+        //            //m_Connection = default(NetworkConnection);
+        //        }
+        //        else if( cmd == NetworkEvent.Type.Disconnect )
+        //        {
+        //            Debug.Log( "Client got disconnected from server" );
+        //            m_Connection = default( NetworkConnection );
+        //        }
+        //    }
+        //}
 
-    private void FixedUpdate()
-    {
-        
-    }
+        //private void FixedUpdate()
+        //{
 
-    void SendPlayerPacket()
-    {
-        if( m_Connection.IsCreated )
-        {
-            UnitBase localPlayer = PlayerController.Instance.localPlayer;
-            if( localPlayer )
-            {
-                PlayerInfoData data = new PlayerInfoData( m_Connection.InternalId );
-                data.transform = localPlayer.transform;
-                if( PlayerController.Instance.HasMessage() )
-                    data.command = PlayerController.Instance.PopMessage();
-                string messageData = JsonUtility.ToJson( data );
+        //}
 
-                Byte[] sendBytes = Encoding.ASCII.GetBytes( messageData );
-                using( var writer = new DataStreamWriter( sendBytes.Length, Allocator.Temp ) )
-                {
-                    writer.Write( sendBytes, sendBytes.Length );
-                    m_Connection.Send( m_Driver, writer );
-                }
-            }
+        //void SendPlayerPacket()
+        //{
+        //    if( m_Connection.IsCreated )
+        //    {
+        //        UnitBase localPlayer = PlayerController.Instance.localPlayer;
+        //        if( localPlayer )
+        //        {
+        //            PlayerInfoData data = new PlayerInfoData( m_Connection.InternalId );
+        //            data.transform = localPlayer.transform;
+        //            if( PlayerController.Instance.HasMessage() )
+        //                data.command = PlayerController.Instance.PopMessage();
+        //            string messageData = JsonUtility.ToJson( data );
 
-        }
-    }
+        //            Byte[] sendBytes = Encoding.ASCII.GetBytes( messageData );
+        //            using( var writer = new DataStreamWriter( sendBytes.Length, Allocator.Temp ) )
+        //            {
+        //                writer.Write( sendBytes, sendBytes.Length );
+        //                m_Connection.Send( m_Driver, writer );
+        //            }
+        //        }
+
+        //    }
+        //}
+    //}
 }
--- Assets/Scripts/Network/NetworkServer.cs
+++ Assets/Scripts/Network/NetworkServer.cs
@@ -6,120 +6,128 @@ using NetworkMessages;
 using System;
 using System.Text;
 
-public class NetworkServer : MonoBehaviour
+namespace OldNetwork
 {
-    public NetworkDriver m_Driver;
-    public ushort serverPort;
-    private NativeList<NetworkConnection> m_Connections;
+    //public class NetworkServer : MonoBehaviour
+    //{
+        //public NetworkDriver m_Driver;
+        //public ushort serverPort;
+        //private NativeList<NetworkConnection> m_Connections;
 
-    void Start ()
-    {
-        m_Driver = NetworkDriver.Create();
-        var endpoint = NetworkEndPoint.AnyIpv4;
-        endpoint.Port = serverPort;
-        if (m_Driver.Bind(endpoint) != 0)
-            Debug.Log("Failed to bind to port " + serverPort);
-        else
-            m_Driver.Listen();
+        //void Start()
+        //{
+        //    m_Driver = NetworkDriver.Create();
+        //    var endpoint = NetworkEndPoint.AnyIpv4;
+        //    endpoint.Port = serverPort;
+        //    if( m_Driver.Bind( endpoint ) != 0 )
+        //        Debug.Log( "Failed to bind to port " + serverPort );
+        //    else
+        //        m_Driver.Listen();
 
-        m_Connections = new NativeList<NetworkConnection>(16, Allocator.Persistent);
-    }
-    void SendToServer(string message, NetworkConnection c){
-        var writer = m_Driver.BeginSend(NetworkPipeline.Null, c);
-        NativeArray<byte> bytes = new NativeArray<byte>(Encoding.ASCII.GetBytes(message),Allocator.Temp);
-        writer.WriteBytes(bytes);
-        m_Driver.EndSend(writer);
-    }
-    public void OnDestroy()
-    {
-        m_Driver.Dispose();
-        m_Connections.Dispose();
-    }
+        //    m_Connections = new NativeList<NetworkConnection>( 16, Allocator.Persistent );
+        //}
+        //void SendToServer( string message, NetworkConnection c )
+        //{
+        //    var writer = m_Driver.BeginSend( NetworkPipeline.Null, c );
+        //    NativeArray<byte> bytes = new NativeArray<byte>( Encoding.ASCII.GetBytes( message ), Allocator.Temp );
+        //    writer.WriteBytes( bytes );
+        //    m_Driver.EndSend( writer );
+        //}
+        //public void OnDestroy()
+        //{
+        //    m_Driver.Dispose();
+        //    m_Connections.Dispose();
+        //}
 
-    void OnConnect(NetworkConnection c){
-        m_Connections.Add(c);
-        Debug.Log("Accepted a connection");
+        //void OnConnect( NetworkConnection c )
+        //{
+        //    m_Connections.Add( c );
+        //    Debug.Log( "Accepted a connection" );
 
-        //// Example to send a handshake message:
-        // HandshakeMsg m = new HandshakeMsg();
-        // m.player.id = c.InternalId.ToString();
-        // SendToServer(JsonUtility.ToJson(m),c);        
-    }
+        //    //// Example to send a handshake message:
+        //    // HandshakeMsg m = new HandshakeMsg();
+        //    // m.player.id = c.InternalId.ToString();
+        //    // SendToServer(JsonUtility.ToJson(m),c);        
+        //}
 
-    void OnData(DataStreamReader stream, int i){
-        NativeArray<byte> bytes = new NativeArray<byte>(stream.Length,Allocator.Temp);
-        stream.ReadBytes(bytes);
-        string recMsg = Encoding.ASCII.GetString(bytes.ToArray());
-        NetworkHeader header = JsonUtility.FromJson<NetworkHeader>(recMsg);
+        //void OnData( DataStreamReader stream, int i )
+        //{
+        //    NativeArray<byte> bytes = new NativeArray<byte>( stream.Length, Allocator.Temp );
+        //    stream.ReadBytes( bytes );
+        //    string recMsg = Encoding.ASCII.GetString( bytes.ToArray() );
+        //    NetworkHeader header = JsonUtility.FromJson<NetworkHeader>( recMsg );
 
-        switch(header.cmd){
-            case Commands.HANDSHAKE:
-            HandshakeMsg hsMsg = JsonUtility.FromJson<HandshakeMsg>(recMsg);
-            Debug.Log("Handshake message received!");
-            break;
-            case Commands.PLAYER_UPDATE:
-            PlayerUpdateMsg puMsg = JsonUtility.FromJson<PlayerUpdateMsg>(recMsg);
-            Debug.Log("Player update message received!");
-            break;
-            case Commands.SERVER_UPDATE:
-            ServerUpdateMsg suMsg = JsonUtility.FromJson<ServerUpdateMsg>(recMsg);
-            Debug.Log("Server update message received!");
-            break;
-            default:
-            Debug.Log("Unrecognized message received!");
-            break;
-        }
-    }
+        //    switch( header.cmd )
+        //    {
+        //        case Commands.HANDSHAKE:
+        //            HandshakeMsg hsMsg = JsonUtility.FromJson<HandshakeMsg>( recMsg );
+        //            Debug.Log( "Handshake message received!" );
+        //            break;
+        //        case Commands.PLAYER_UPDATE:
+        //            PlayerUpdateMsg puMsg = JsonUtility.FromJson<PlayerUpdateMsg>( recMsg );
+        //            Debug.Log( "Player update message received!" );
+        //            break;
+        //        case Commands.SERVER_UPDATE:
+        //            ServerUpdateMsg suMsg = JsonUtility.FromJson<ServerUpdateMsg>( recMsg );
+        //            Debug.Log( "Server update message received!" );
+        //            break;
+        //        default:
+        //            Debug.Log( "Unrecognized message received!" );
+        //            break;
+        //    }
+        //}
 
-    void OnDisconnect(int i){
-        Debug.Log("Client disconnected from server");
-        m_Connections[i] = default(NetworkConnection);
-    }
+        //void OnDisconnect( int i )
+        //{
+        //    Debug.Log( "Client disconnected from server" );
+        //    m_Connections[i] = default( NetworkConnection );
+        //}
 
-    void Update ()
-    {
-        m_Driver.ScheduleUpdate().Complete();
+        //void Update()
+        //{
+        //    m_Driver.ScheduleUpdate().Complete();
 
-        // CleanUpConnections
-        for (int i = 0; i < m_Connections.Length; i++)
-        {
-            if (!m_Connections[i].IsCreated)
-            {
-                m_Connections.RemoveAtSwapBack(i);
-                --i;
-            }
-        }
+        //    // CleanUpConnections
+        //    for( int i = 0; i < m_Connections.Length; i++ )
+        //    {
+        //        if( !m_Connections[i].IsCreated )
+        //        {
+        //            m_Connections.RemoveAtSwapBack( i );
+        //            --i;
+        //        }
+        //    }
 
-        // AcceptNewConnections
-        NetworkConnection c = m_Driver.Accept();
-        while (c  != default(NetworkConnection))
-        {            
-            OnConnect(c);
+        //    // AcceptNewConnections
+        //    NetworkConnection c = m_Driver.Accept();
+        //    while( c != default( NetworkConnection ) )
+        //    {
+        //        OnConnect( c );
 
-            // Check if there is another new connection
-            c = m_Driver.Accept();
-        }
+        //        // Check if there is another new connection
+        //        c = m_Driver.Accept();
+        //    }
 
-        DataStreamReader stream;
-        for (int i = 0; i < m_Connections.Length; i++)
-        {
-            Assert.IsTrue(m_Connections[i].IsCreated);
-            
-            NetworkEvent.Type cmd;
-            cmd = m_Driver.PopEventForConnection(m_Connections[i], out stream);
-            while (cmd != NetworkEvent.Type.Empty)
-            {
-                if (cmd == NetworkEvent.Type.Data)
-                {
-                    OnData(stream, i);
-                }
-                else if (cmd == NetworkEvent.Type.Disconnect)
-                {
-                    OnDisconnect(i);
-                }
+        //    DataStreamReader stream;
+        //    for( int i = 0; i < m_Connections.Length; i++ )
+        //    {
+        //        Assert.IsTrue( m_Connections[i].IsCreated );
 
-                cmd = m_Driver.PopEventForConnection(m_Connections[i], out stream);
-            }
-        }
-    }
+        //        NetworkEvent.Type cmd;
+        //        cmd = m_Driver.PopEventForConnection( m_Connections[i], out stream );
+        //        while( cmd != NetworkEvent.Type.Empty )
+        //        {
+        //            if( cmd == NetworkEvent.Type.Data )
+        //            {
+        //                OnData( stream, i );
+        //            }
+        //            else if( cmd == NetworkEvent.Type.Disconnect )
+        //            {
+        //                OnDisconnect( i );
+        //            }
+
+        //            cmd = m_Driver.PopEventForConnection( m_Connections[i], out stream );
+        //        }
+        //    }
+        //}
+    //}
 }
